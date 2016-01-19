95a746d15986ece6ffea0be3e7e44c2e7c2658ea
Andy Sayler <andy.sayler@gmail.com>
2016-01-18 20:57:21 -0700

    Removed nexted list from key functions

--- api_accesscontrol/api.py
+++ api_accesscontrol/api.py
@@ -163,14 +163,14 @@ def get_root():
 def get_pub_cacert():
 
     app.logger.debug("GET PUB CACERT")
-    json_out = {_KEY_CACERT: [flask.g.srv_ac.ca_crt]}
+    json_out = {_KEY_CACERT: flask.g.srv_ac.ca_crt}
     return flask.jsonify(json_out)
 
 @app.route("/{}/{}/".format(_EP_PUBLIC, _KEY_SIGKEY), methods=['GET'])
 def get_pub_sigkey():
 
     app.logger.debug("GET PUB SIGKEY")
-    json_out = {_KEY_SIGKEY: [flask.g.srv_ac.sigkey_pub]}
+    json_out = {_KEY_SIGKEY: flask.g.srv_ac.sigkey_pub}
     return flask.jsonify(json_out)
 
 
